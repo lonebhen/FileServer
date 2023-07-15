f93a5cf112936db6fa75bf3b89d9a5f45933e5b0
lonebhen <ben.angmortey@gmail.com>
2023-07-15 22:45:38 +0000

    fixed the mail sending so now users can receive mimetypes

--- server/send_mail.py
+++ server/send_mail.py
@@ -1,70 +1,55 @@
-from django.core.mail import send_mail, EmailMessage
-from .models import Files
+import os
+import mimetypes
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from django.shortcuts import get_object_or_404
-import boto3
-from botocore.exceptions import NoCredentialsError
-from rest_framework.response import Response
-
+from django.core.mail import EmailMessage
+from .models import Files
+from storages.backends.s3boto3 import S3Boto3Storage
 
 def send_file_email(id: int, email: str):
-    # file_object = get_object_or_404(Files, pk= id)
-
-    # file = file_object.file
-
-    # email_subject = "File From Fileserver"
-    # email_body = "File you requested from fileserver app"
-
-    # email_from = "ben.angmortey@gmail.com"
-
-    # email_to = email
-
-    # email = EmailMessage(email_subject, email_body, email_from, [email_to] )
-
-    # try:
-    #     s3_client = boto3.client('s3')
-    #     response = s3_client.get_object(Bucket='ben-fileserver', Key=str(file))
-    #     file_data = response['Body'].read()
-
-    #     # Attach the file to the email
-    #     email.attach(file.name, file_data, 'application/octet-stream')
-
-    #     # Send the email
-    #     email.send()
-
-    # except NoCredentialsError:
-    #     return Response({"message": "Failed to send email. Amazon S3 credentials not found."})
-
-    # except Exception as e:
-    #     return Response({"message": "Failed to send email. Error: " + str(e)})
-
-    # # file_data = file.read()
-
-    # # email.attach('filename.ext', file_data, 'application/octet-stream')
-
-    # # email.send()
-
-
-    file_object = get_object_or_404(Files, pk= id)
-
+    file_object = get_object_or_404(Files, pk=id)
     file_name = file_object.name
 
     email_subject = "File From Fileserver"
     email_body = "File you requested from fileserver app"
-
     email_from = "ben.angmortey@gmail.com"
-
     email_to = email
 
+    # Create the email message
+    email = MIMEMultipart()
+    email["Subject"] = email_subject
+    email["From"] = email_from
+    email["To"] = email_to
 
-    email = EmailMessage(email_subject, email_body, email_from, [email_to] )
+    # Retrieve file data from Amazon S3
+    file_path = file_object.file.name
+    storage = S3Boto3Storage()
+    file_data = storage.open(file_path, "rb").read()
 
-    # send_mail(email_subject, email_body, email_from, [email_to],
-    #           attachments=[(file_name, file_object.file, 'application/octet-stream')])
+    # Attach the file to the email
+    content_type, encoding = mimetypes.guess_type(file_path)
+    if content_type is None or encoding is not None:
+        content_type = "application/octet-stream"
 
+    maintype, subtype = content_type.split("/", 1)
+    attachment = MIMEBase(maintype, subtype)
+    attachment.set_payload(file_data)
+    encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", "attachment", filename=file_name)
+    email.attach(attachment)
 
-    file_content = file_object.file.read()
-    email.attach(file_name, file_content, 'application/octet-stream')
+    # # Send the email
+    # with smtplib.SMTP(host="your-smtp-host", port=587) as server:
+    #     server.starttls()
+    #     server.login("your-email@example.com", "your-email-password")
+    #     server.sendmail(email_from, email_to, email.as_string())
+
 
     # Send the email
-    email.send()
-
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+        server.starttls()
+        server.login("ben.angmortey@gmail.com", "uuyhmqzqikfimosg")
+        server.sendmail(email_from, email_to, email.as_string())
